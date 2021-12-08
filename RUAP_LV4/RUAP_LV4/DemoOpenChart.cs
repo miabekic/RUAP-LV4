ba8be66e34daad27ead63b5da91327a32470fb86
miabekic <mia.zima.4@gmail.com>
2021-12-08 21:12:02 +0100

    DemoOpenChart-AllTests

---
+++ RUAP_LV4/RUAP_LV4/DemoOpenChart.cs
@@ -0,0 +1,187 @@
+using NUnit.Framework;
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace RUAP_LV4
+{
+    [TestFixture]
+    public class DemoOpenChart
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--start-maximized");
+            driver = new ChromeDriver(options);
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+            
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TestAddNewUser()
+        {
+            driver.Navigate().GoToUrl("https://demo.opencart.com/");
+            driver.FindElement(By.LinkText("My Account")).Click();
+            driver.FindElement(By.LinkText("Register")).Click();
+            driver.FindElement(By.Id("input-firstname")).Click();
+            driver.FindElement(By.Id("input-firstname")).Clear();
+            driver.FindElement(By.Id("input-firstname")).SendKeys("TT");
+            driver.FindElement(By.Id("input-lastname")).Click();
+            driver.FindElement(By.Id("input-lastname")).Clear();
+            driver.FindElement(By.Id("input-lastname")).SendKeys("BB");
+            driver.FindElement(By.Id("input-email")).Click();
+            driver.FindElement(By.Id("input-email")).Clear();
+            driver.FindElement(By.Id("input-email")).SendKeys("mia.zima.4@gmail.com");
+            driver.FindElement(By.Id("input-telephone")).Click();
+            driver.FindElement(By.Id("input-telephone")).Clear();
+            driver.FindElement(By.Id("input-telephone")).SendKeys("000");
+            driver.FindElement(By.Id("input-password")).Click();
+            driver.FindElement(By.Id("input-password")).Clear();
+            driver.FindElement(By.Id("input-password")).SendKeys("1234");
+            driver.FindElement(By.Id("input-confirm")).Click();
+            driver.FindElement(By.Id("input-confirm")).Clear();
+            driver.FindElement(By.Id("input-confirm")).SendKeys("1234");
+            driver.FindElement(By.Name("agree")).Click();
+            driver.FindElement(By.XPath("//input[@value='Continue']")).Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.LinkText("Continue")).Click();
+        }
+
+        [Test]
+        public void TestLogin()
+        {
+            driver.Navigate().GoToUrl("https://demo.opencart.com/");
+            driver.FindElement(By.XPath("//div[@id='top-links']/ul/li[2]/a/i")).Click();
+            driver.FindElement(By.LinkText("Login")).Click();
+            driver.FindElement(By.Id("input-email")).Click();
+            driver.FindElement(By.Id("input-email")).Clear();
+            driver.FindElement(By.Id("input-email")).SendKeys("mia.zima.4@gmail.com");
+            driver.FindElement(By.Id("input-password")).Click();
+            driver.FindElement(By.Id("input-password")).Clear();
+            driver.FindElement(By.Id("input-password")).SendKeys("1234");
+            Thread.Sleep(2000);
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+            Thread.Sleep(2000);
+        }
+
+        [Test]
+        public void TestAddToChart()
+        {
+            driver.Navigate().GoToUrl("https://demo.opencart.com/");
+            driver.FindElement(By.XPath("//div[@id='content']/div[2]/div[2]/div/div[3]/button/span")).Click();
+            Thread.Sleep(2000);
+        }
+
+        [Test]
+        public void TestAddToWishList()
+        {
+            driver.Navigate().GoToUrl("https://demo.opencart.com/");
+            driver.FindElement(By.XPath("//div[@id='top-links']/ul/li[2]/a/span")).Click();
+            driver.FindElement(By.LinkText("Login")).Click();
+            driver.FindElement(By.Id("input-email")).Click();
+            driver.FindElement(By.Id("input-email")).Clear();
+            driver.FindElement(By.Id("input-email")).SendKeys("mia.zima.4@gmail.com");
+            driver.FindElement(By.Id("input-password")).Click();
+            driver.FindElement(By.Id("input-password")).Clear();
+            driver.FindElement(By.Id("input-password")).SendKeys("1234");
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+            driver.FindElement(By.LinkText("Your Store")).Click();
+            driver.FindElement(By.XPath("//div[@id='content']/div[2]/div/div/div[3]/button[2]/i")).Click();
+            Thread.Sleep(5000);
+        }
+
+        [Test]
+        public void TestChangeData()
+        {
+            driver.Navigate().GoToUrl("https://demo.opencart.com/");
+            driver.FindElement(By.XPath("//div[@id='top-links']/ul/li[2]/a/i")).Click();
+            driver.FindElement(By.LinkText("Login")).Click();
+            driver.FindElement(By.Id("input-email")).Click();
+            driver.FindElement(By.Id("input-email")).Clear();
+            driver.FindElement(By.Id("input-email")).SendKeys("mia.zima.4@gmail.com");
+            driver.FindElement(By.Id("input-password")).Click();
+            driver.FindElement(By.Id("input-password")).Clear();
+            driver.FindElement(By.Id("input-password")).SendKeys("1234");
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.LinkText("Edit your account information")).Click();
+            driver.FindElement(By.Id("input-telephone")).Click();
+            driver.FindElement(By.Id("input-telephone")).Clear();
+            driver.FindElement(By.Id("input-telephone")).SendKeys("0000");
+            driver.FindElement(By.XPath("//input[@value='Continue']")).Click();
+            Thread.Sleep(2000);
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
